a6efd3099ae9bfc4be0dba25887553d2e3cd699e
Robert W. Ellenberg <rwe24g@gmail.com>
2019-08-14 19:47:25 -0400

    Add publish completion

--- git-script-completion.bash
+++ git-script-completion.bash
@@ -5,3 +5,9 @@ _git_update_branch ()
   # you can return anything here for the autocompletion for example all the branches
   __gitcomp_nl "$(__git_refs)"
 }
+
+_git_publish ()
+{
+  # you can return anything here for the autocompletion for example all the branches
+  __gitcomp_nl "$(__git_refs)"
+}
